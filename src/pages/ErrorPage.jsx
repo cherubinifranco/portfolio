345c8237a41d4aae11f19577ac1aac759efb8dc3
cherubinifranco <cherubini.franco@hotmail.com>
2025-11-12 07:16:43 -0300

    ferat: basic 404 page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,10 +1,24 @@
+import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 
 export default function ErrorPage() {
   return (
-    <main className="overflow-hidden">
-      <h1>No where no to my friendo</h1>
-      <a href="/">Main Page</a>
+    <main>
+      <div className="overflow-hidden w-full p-8 h-screen flex flex-col items-center justify-center text-white">
+        <h1 className="text-[60px]">😔</h1>
+        <h1 className="text-2xl">Error 404 - Page Not Found</h1>
+        <p className="text-center mt-4">
+          This page you are looking for does not exists or might be redirected
+          to another url
+        </p>
+        <a
+          href="/"
+          className="font-semibold px-6 py-3 border-2 border-gray-600 rounded-lg mt-8 hover:border-blue-600 transition duration-300"
+        >
+          Go To Homepage
+        </a>
+      </div>
+      <Footer />
     </main>
   );
 }
