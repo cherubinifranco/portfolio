0abacf037ebd6c5b3c17c41fdfd7c26ea35569ea
Cherubini Franco <cherubini.franco@hotmail.com>
2025-11-15 05:29:37 -0300

    feat: Setup routes for project pages

--- src/App.js
+++ src/App.js
@@ -2,18 +2,21 @@ import "./App.css";
 import ErrorPage from "./pages/ErrorPage";
 import IndexPage from "./pages/IndexPage";
 import BeanItPage from "./pages/BeanPage.jsx";
-
-import {BrowserRouter, Route, Routes } from "react-router-dom";
+import BchmapPage from "./pages/BchmapPage.jsx";
+import ProjectsPage from "./pages/ProjectsPage.jsx";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-      <BrowserRouter>
-        <Routes>
-          <Route path="*" element={<ErrorPage />} />
-          <Route path="/" element={<IndexPage />} />
-          <Route path="/beanit" element={<BeanItPage />} />
-        </Routes>
-      </BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route path="*" element={<ErrorPage />} />
+        <Route path="/" element={<IndexPage />} />
+        <Route path="/projects" element={<ProjectsPage />} />
+        <Route path="/projects/beanit" element={<BeanItPage />} />
+        <Route path="/projects/bchmap" element={<BchmapPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
