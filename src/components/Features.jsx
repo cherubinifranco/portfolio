9be581011c8668ec30c318e8edd6a74fa6c738f3
Cherubini Franco <cherubini.franco@hotmail.com>
2025-11-15 05:31:19 -0300

    feat: Updated Features looks

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,22 +1,25 @@
 import { doublesquareIcon } from "../files/icons";
+import RevealOnScroll from "./RevealOnScroll";
 
-export default function Features({
-  features = ["First Feature", "Second Feature", "Third Feature"]
-}) {
+export default function Features({ features = ["First Feature", "Second Feature", "Third Feature"], img = "./assets/test-1.png" }) {
   return (
-    <section className="flex flex-col w-full mt-32 px-12 max-w-[1400px] mx-auto pb-10">
-      <h1 className="mx-auto text-4xl pb-10">Features</h1>
-      <div className="flex flex-col justify-center items-center lg:flex-row gap-10">
-        <img className="w-full md:w-1/2 gap-6 aspect-video border object-fit"></img>
-        <div className="">
-          {features.map((feature) => (
-            <article className="py-5 flex gap-4 text-blue-400">
-              {doublesquareIcon}
-              <span className="text-white">{feature}</span>
-            </article>
-          ))}
+    <RevealOnScroll>
+      <section className="flex flex-col w-full mt-16 px-12 max-w-[1400px] mx-auto pb-10">
+        <h1 className="mx-auto text-4xl pb-10">Features</h1>
+
+        <div className="flex flex-col justify-center items-center lg:flex-row gap-10">
+          <img className="w-full lg:w-1/2 aspect-video border rounded-xl object-contain" src={img} alt="Feature image" />
+
+          <div className="lg:w-1/2">
+            {features.map((feature, idx) => (
+              <article key={idx} className="py-5 flex gap-4 text-blue-400">
+                {doublesquareIcon}
+                <span className="text-white">{feature}</span>
+              </article>
+            ))}
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </RevealOnScroll>
   );
 }
