9ca3f685ba0f53518f29202f225352f1dd671810
cherubinifranco <cherubini.franco@hotmail.com>
2025-11-15 11:08:18 -0300

    feat: Updated Footer and added more links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,26 +1,85 @@
+import { logoIconFull } from "../files/icons";
+import downloadFile from "../files/resume.pdf";
+
 export default function Footer() {
   return (
-    <section
-      id="#footer"
-      className="flex flex-col items-center gap-8 sm:gap-10 sm:flex-row sm:justify-center p-10 font-semibold bg-secondbg w-full text-alwhite text-sm"
-    >
-      <img src="" alt="" className="w-40 h-40 mx-10" />
-
-      <div className="flex flex-col font-thin gap-3 w-40">
-        <h1 className="font-semibold pb-2">Links Utiles</h1>
-        <a href="" className="hover:text-blue-400">Proyectos?</a>
-        <a href="" className="hover:text-blue-400">Link 2</a>
-        <a href="" className="hover:text-blue-400">Link 3</a>
-      </div>
-
-      <div className="flex flex-col font-thin gap-3 w-40">
-        <h1 className="font-semibold pb-2">Redes</h1>
-        <a href="" className="hover:text-blue-400">
-          LinkedIn
+    <footer className="bg-secondbg">
+      <div
+        id="#footer"
+        className="flex flex-col gap-8 sm:gap-10 items-center sm:items-start sm:flex-row sm:justify-center p-10 w-full text-alwhite"
+      >
+        <a
+          className="w-32 h-32 mx-10 opacity-90 hover:opacity-100 transition"
+          href="/"
+        >
+          {logoIconFull}
         </a>
-        <a href="" className="hover:text-blue-400">Github</a>
-        <a href="" className="hover:text-blue-400">Email</a>
+
+        <article className="flex flex-col gap-2 sm:w-40 w-auto items-center sm:items-start">
+          <h1 className="text-sm font-semibold tracking-wide uppercase text-gray-300 pb-1">
+            Site
+          </h1>
+          <a
+            href="/#banner"
+            className="text-xs text-gray-400 hover:text-blue-400 transition"
+          >
+            Home
+          </a>
+          <a
+            href="/projects"
+            className="text-xs text-gray-400 hover:text-blue-400 transition"
+          >
+            Projects
+          </a>
+        </article>
+
+        <article className="flex flex-col gap-2 sm:w-40 w-auto items-center sm:items-start">
+          <h1 className="text-sm font-semibold tracking-wide uppercase text-gray-300 pb-1">
+            Connect
+          </h1>
+          <a
+            href="https://www.linkedin.com/in/cherubinifranco"
+            target="_blank"
+            className="text-xs text-gray-400 hover:text-blue-400 transition"
+          >
+            LinkedIn
+          </a>
+          <a
+            href="https://www.github.com/cherubinifranco"
+            target="_blank"
+            className="text-xs text-gray-400 hover:text-blue-400 transition"
+          >
+            GitHub
+          </a>
+          <a
+            href="mailto:cherubini.franco@hotmail.com"
+            target="_blank"
+            className="text-xs text-gray-400 hover:text-blue-400 transition"
+          >
+            Email
+          </a>
+          <a
+            href="https://www.fiverr.com/francocherubini"
+            target="_blank"
+            className="text-xs text-gray-400 hover:text-blue-400 transition"
+          >
+            Fiverr
+          </a>
+          <a
+            href={downloadFile}
+            download="Resume"
+            className="text-xs text-gray-400 hover:text-blue-400 transition"
+          >
+            Resume
+          </a>
+        </article>
       </div>
-    </section>
+
+      <div className="border-t border-gray-600 w-3/4 mx-auto" />
+
+      <div className="text-gray-500 text-center py-4 text-[10px] tracking-wide">
+        2025 • Built by Franco Cherubini
+      </div>
+    </footer>
   );
 }
